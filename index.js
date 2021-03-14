a109f826de1bac24729ace16806ff771dbd41a47
bonificial <mboniface107@gmail.com>
2021-03-14 14:24:14 +0300

    updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-port = 3000;
+port = process.env.PORT || 3000;
 io.on('connection', socket=>{
     console.log('A user Connected')
     socket.on('chatMessage', message=>{
@@ -17,3 +17,5 @@ io.on('connection', socket=>{
 server.listen(port,()=>{
     console.log('Server Running', port)
 })
+
+//git add . && git commit -m "updates" && git push -u origin master
