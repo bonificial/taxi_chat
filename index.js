f79feb4cf05fb5899086679b7260ad9ec4562aaa
bonificial <mboniface107@gmail.com>
2021-04-11 09:51:02 +0300

    message

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ fb.initializeFirebaseDB();
 // fh.readFile('me.json');
 //storeData({contact:'0700', name:'Bonificial'},'me.json');
 io.on('connection', socket => {
-    console.log(socket.id, ' has   Connected')
+    console.log(socket.id, ' has   Connected');
+
 
     socket.on('joinRoom', ({userID, roomID}) => {
         socket.join(roomID);
@@ -24,21 +25,22 @@ io.on('connection', socket => {
     })
 
     socket.on('chatMessage', message => {
-        console.log(`${message.username} sent a new message ${message.message.text}`);
+        console.log(`${message.user} sent a new message ${message.chatMessage}`);
         io.emit('chatMessageResponse', message);
-let threadkey = message.threadkey;
-let messageObj = message.message;
-let filename = threadkey + ".json";
-if(fh.FileExistsOnS3(filename)){
-    fh.updateFile(filename,data);
-}else{
-    fh.storeData({threadkey:threadkey, chats:[messageObj]}, threadkey + ".json");
-}
+        let threadkey = message.threadkey;
+        let messageObj = message.message;
+        let filename = threadkey + ".json";
+
+     /*   if (fh.FileExistsOnS3(filename)) {
+            fh.updateFile(filename, data);
+        } else {
+            fh.storeData({threadkey: threadkey, chats: [messageObj]}, threadkey + ".json");
+        }*/
 
     })
     socket.on('disconnect', () => {
 
-        console.log(socket.id , ' has disconnected')
+        console.log(socket.id, ' has disconnected')
         socket.emit('message', socket.id + ' has left the chat')
 
     })
@@ -46,20 +48,20 @@ if(fh.FileExistsOnS3(filename)){
 server.listen(port, () => {
     console.log('Server Running', port)
 })
-app.get('/read/:filename', function(req, res) {
+app.get('/read/:filename', function (req, res) {
     let filename = req.params.filename + '.json';
-console.log('Filename is ', filename)
-    fh.readFile(filename ).then(response=>{
+    console.log('Filename is ', filename)
+    fh.readFile(filename).then(response => {
         console.log(response)
         //res.type('json');
 //console.log(JSON.parse(response))
-        res.send( JSON.parse(response));
+        res.send(JSON.parse(response));
     })
 
 });
-app.get('/update/:filename/:town', function(req, res) {
+app.get('/update/:filename/:town', function (req, res) {
     let filename = req.params.filename + '.json';
-    let data =     {
+    let data = {
         "_id": "xXZJpTerOUcKfJSHbme16IcCxUs2_1610542057194",
         "createdAt": "Wednesday, January 13, 2021 3:47 PM",
         "key": "xXZJpTerOUcKfJSHbme16IcCxUs2_1610542057194",
@@ -70,8 +72,8 @@ app.get('/update/:filename/:town', function(req, res) {
         }
     };
     // console.log(filename);
-    fh.updateFile(filename,data).then(resp=>{
-        res.send( 'File Update Success');
+    fh.updateFile(filename, data).then(resp => {
+        res.send('File Update Success');
     })
 
 
